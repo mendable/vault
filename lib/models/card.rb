6510822d89c046d8ac185d1f3ee979c4e758be4e
Jason <mail@jasonlambert.co.uk>
2011-02-26 23:53:42 +0000

    validations for credit card meta-info

--- lib/models/card.rb
+++ lib/models/card.rb
@@ -1,3 +1,28 @@
 class Card < ActiveRecord::Base
+
+  # Number Validations
   validates :number, :presence => true
+  validates :issue_number, :numericality => true, :allow_nil => true
+
+  # Date Validations
+  validates :month, :year, :presence => true, :numericality => true
+  validates :start_month, :inclusion => {:in => [nil, 1,2,3,4,5,6,7,8,9,10,11,12] }
+  validates :start_year, :numericality => true, :allow_nil => true
+
+  validates_each :month, :year do |record, attribute, value|
+    if record.year && record.month then
+      if Date.parse("#{record.year}-#{record.month}-01") < Time.now.to_date.beginning_of_month then
+        record.errors[attribute] << "Cannot be in the past"
+      end
+    end
+  end
+
+  validates_each :start_month, :start_year do |record, attribute, value|
+    if record.start_month && record.start_year then
+      if Date.parse("#{record.start_year}-#{record.start_month}-01") > Time.now.to_date.beginning_of_month then
+        record.errors[attribute] << "Cannot be in the future"
+      end
+    end
+  end
+
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -12,5 +12,108 @@ describe Card do
   it { should have_db_column(:updated_at).of_type(:datetime) }
 
   it { should validate_presence_of(:number) }
+  it { should validate_presence_of(:month) }
+  it { should validate_presence_of(:year) }
+
+  it { should validate_numericality_of(:issue_number) }
+  it { should allow_value(nil).for(:issue_number) }
+
+  it { should validate_numericality_of(:month) }
+  it { should validate_numericality_of(:year) }
+  it { should validate_numericality_of(:start_year) }
+  it { should allow_value(nil).for(:start_month) }
+  it { should allow_value(nil).for(:start_year) }
+
+  context "Expiry Date Validation" do
+    before(:each) do
+      @card = Card.new
+    end
+
+    specify "when todays month and year" do
+      @card.year = Time.now.year
+      @card.month = Time.now.month
+      @card.valid?
+      @card.errors[:year].should be_empty
+      @card.errors[:month].should be_empty
+    end
+
+    specify "when in the future by 1 month" do
+      @card.year = Time.now.year
+      @card.month = Time.now.month + 1 #FIXME: Test will fail in December months
+      @card.valid?
+      @card.errors[:year].should be_empty
+      @card.errors[:month].should be_empty
+    end
+
+    specify "when in the future by a year" do
+      @card.year = Time.now.year + 1
+      @card.month = Time.now.month
+      @card.valid?
+      @card.errors[:year].should be_empty
+      @card.errors[:month].should be_empty
+    end
+
+    specify "when a month in the past" do
+      @card.year = Time.now.year
+      @card.month = Time.now.month - 1 #FIXME: Test will fail in January months
+      @card.valid?
+      @card.errors[:year].should_not be_empty
+      @card.errors[:month].should_not be_empty
+    end
+
+    specify "when todays month and year in the past" do
+      @card.year = Time.now.year - 1
+      @card.month = Time.now.month
+      @card.valid?
+      @card.errors[:year].should_not be_empty
+      @card.errors[:month].should_not be_empty
+    end
+  end
+
+  context "Start Date Validation" do
+    before(:each) do
+      @card = Card.new
+    end
+
+    specify "when todays month and year" do
+      @card.start_year = Time.now.year
+      @card.start_month = Time.now.month
+      @card.valid?
+      @card.errors[:start_year].should be_empty
+      @card.errors[:start_month].should be_empty
+    end
+
+    specify "when in the future by 1 month" do
+      @card.start_year = Time.now.year
+      @card.start_month = Time.now.month + 1 #FIXME: Test will fail in December months
+      @card.valid?
+      @card.errors[:start_year].should_not be_empty
+      @card.errors[:start_month].should_not be_empty
+    end
+
+    specify "when in the future by a year" do
+      @card.start_year = Time.now.year + 1
+      @card.start_month = Time.now.month
+      @card.valid?
+      @card.errors[:start_year].should_not be_empty
+      @card.errors[:start_month].should_not be_empty
+    end
+
+    specify "when a month in the past" do
+      @card.start_year = Time.now.year
+      @card.start_month = Time.now.month - 1 #FIXME: Test will fail in January months
+      @card.valid?
+      @card.errors[:start_year].should be_empty
+      @card.errors[:start_month].should be_empty
+    end
+
+    specify "when todays month and year in the past" do
+      @card.start_year = Time.now.year - 1
+      @card.start_month = Time.now.month
+      @card.valid?
+      @card.errors[:start_year].should be_empty
+      @card.errors[:start_month].should be_empty
+    end
+  end
 
 end
